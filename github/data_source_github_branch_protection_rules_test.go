8a641a90c78b2dd291fbde795b38cd8aba31ee65
Manu Magalhaes <89524998+magmanu@users.noreply.github.com>
2023-08-01 19:45:42 +0100

    chore: add more tests

--- github/data_source_github_branch_protection_rules_test.go
+++ github/data_source_github_branch_protection_rules_test.go
@@ -84,6 +84,20 @@ func TestAccGithubBranchProtectionRulesDataSource(t *testing.T) {
 			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.blocks_creations", "false"),
 			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.dismisses_stale_reviews", "false"),
 			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.is_admin_enforced", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.lock_allows_fetch_and_merge", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.lock_branch", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.require_last_push_approval", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.required_approving_review_count", "1"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.requires_approving_reviews", "true"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.requires_codeowner_reviews", "true"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.requires_commit_signatures", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.requires_conversation_resolution", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.requires_linear_history", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.requires_deployments", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.requires_status_checks", "true"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.requires_strict_status_checks", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.restricts_pushes", "false"),
+			resource.TestCheckResourceAttr("data.github_branch_protection_rules.all", "rules.0.restricts_review_dismissals", "false"),
 		)
 
 		testCase := func(t *testing.T, mode string) {
